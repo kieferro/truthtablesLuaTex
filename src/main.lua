d4848c2f1fc9ce59581f48c2e8225b7fd0f5bf52
kieferro <81773954+kieferro@users.noreply.github.com>
2022-09-24 22:56:49 +0200

    Add import of main script by unit tests

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,3 @@
 function evaluateExpression (exp)
     return true
 end
-
-logicalExpression = "((NOT (NOT p)) OR (q AND b))"
-print(evaluateExpression(logicalExpression))
--- src/test.lua
+++ src/test.lua
@@ -1,7 +1,22 @@
+function get_script_path(script_name)
+    local path = debug.getinfo(2, "S").source:sub(2)
+    local res_windows = path:match("(.*[/\\])")
+    local res_unix = path:match("(.*/)")
+
+    if res_windows ~= nil then
+        return res_windows .. script_name
+    elseif res_unix ~= nil then
+        return res_unix .. script_name
+    else
+        return script_name
+    end
+end
+
+require(get_script_path("main"))
 luaunit = require("luaunit")
 
 function test()
-    luaunit.assertEquals(1,1)
+    luaunit.assertEquals(true, evaluateExpression(""))
 end
 
 -- Start unit testing and close with status code
